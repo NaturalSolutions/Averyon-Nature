0df01031bee2175894fd387d02f8463c3b8cbdc7
gitkyo <p.delaunay1@gmail.com>
2017-03-29 14:29:58 +0200

    edit controller gallery + list event

--- modules/gallery/src/Controller/galleryController.php
+++ modules/gallery/src/Controller/galleryController.php
@@ -14,7 +14,28 @@ class galleryController extends ControllerBase {
 	public function content() {
 
 		$data = [];
-		$data['test'] = "test";
+
+		/*
+		* Get pictures's gallerie
+		*/
+		$query = db_query("
+			SELECT f.uri, g.field_gallery_alt,
+			g.field_gallery_title
+			from file_managed f
+			join node__field_gallery g
+			on f.fid = g.field_gallery_target_id
+		");
+
+		$pictures = $query->fetchAll();
+
+		foreach ($pictures as $key => $picture) {
+
+			$picture->uri = entity_load('image_style', '500_par_350')->buildUrl($picture->uri);
+
+		}
+
+		// add to global var data
+		$data['pictures'] = $pictures;
 
 		/*
 		* Return all data
