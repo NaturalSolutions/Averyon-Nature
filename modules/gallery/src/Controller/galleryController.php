5bc15e55a3beba0a6d65f40421964188e8cd120b
gitkyo <p.delaunay1@gmail.com>
2017-03-29 15:33:37 +0200

    add taxon's tag in ens controller page and add many data in gallery controller page

--- modules/gallery/src/Controller/galleryController.php
+++ modules/gallery/src/Controller/galleryController.php
@@ -16,26 +16,101 @@ class galleryController extends ControllerBase {
 		$data = [];
 
 		/*
-		* Get pictures's gallerie
+		* Get ENS pictures's gallerie
 		*/
 		$query = db_query("
 			SELECT f.uri, g.field_gallery_alt,
-			g.field_gallery_title
+			g.field_gallery_title,d.nid, e.field_thematique_ens_target_id
 			from file_managed f
 			join node__field_gallery g
 			on f.fid = g.field_gallery_target_id
+			join node_field_data d
+			on d.nid = g.entity_id
+			join node__field_thematique_ens e
+			on e.entity_id = d.nid
+			where d.type = 'ens'
+			ORDER BY RAND()
 		");
 
-		$pictures = $query->fetchAll();
+		$picturesENS = $query->fetchAll();
 
-		foreach ($pictures as $key => $picture) {
+		foreach ($picturesENS as $key => $pictureENS) {
 
-			$picture->uri = entity_load('image_style', '500_par_350')->buildUrl($picture->uri);
+			//Add alias path
+			$path_alias = \Drupal::service('path.alias_manager')->getAliasByPath("/node/".$pictureENS->nid, $langcode);
+			$path_alias = ltrim($path_alias, '/');
+			$pictureENS->url_alias = $path_alias;
+
+			if($pictureENS->field_gallery_alt == 'faune' || $pictureENS->field_gallery_alt == 'flore' || $pictureENS->field_gallery_alt == 'paysage' || $pictureENS->field_gallery_alt == 'patrimoine'){
+
+				$pictureENS->tag = $pictureENS->field_gallery_alt;
+
+			}
+			else{
+
+				$query = db_query("
+					SELECT d.title
+					FROM aveyron.node_field_data d
+					where d.nid = $pictureENS->field_thematique_ens_target_id
+				");
+
+				$tagENS = $query->fetchAll();
+
+				$pictureENS->tag = strtolower($tagENS[0]->title);
+
+			}
+
+			// convert style
+			$pictureENS->uri = entity_load('image_style', '900_par_600')->buildUrl($pictureENS->uri);
+
+			// remove useless properties
+			unset($pictureENS->field_thematique_ens_target_id);
+			unset($pictureENS->nid);
 
 		}
 
 		// add to global var data
-		$data['pictures'] = $pictures;
+		$data['pictures'] = $picturesENS;
+
+		/*
+		* Get Taxons pictures's gallerie
+		*/
+		$query = db_query("
+			SELECT f.uri, g.field_gallery_alt,
+			g.field_gallery_title,d.nid, t.field_tag_value as tag
+			from file_managed f
+			join node__field_gallery g
+			on f.fid = g.field_gallery_target_id
+			join node_field_data d
+			on d.nid = g.entity_id
+			join node__field_tag t
+			on t.entity_id = d.nid
+			where d.type = 'taxon'
+			ORDER BY RAND()
+		");
+
+		$picturesTaxons = $query->fetchAll();
+
+		foreach ($picturesTaxons as $key => $pictureTaxon) {
+
+			//Add alias path
+			$path_alias = \Drupal::service('path.alias_manager')->getAliasByPath("/node/".$pictureTaxon->nid, $langcode);
+			$path_alias = ltrim($path_alias, '/');
+			$pictureTaxon->url_alias = $path_alias;
+
+			// convert style
+			$pictureTaxon->uri = entity_load('image_style', '900_par_600')->buildUrl($pictureTaxon->uri);
+
+			// remove useless properties
+			unset($pictureTaxon->nid);
+
+			// add in the main array
+			array_push($data['pictures'], $pictureTaxon);
+		}
+
+
+		// random sort
+		shuffle($data['pictures']);
 
 		/*
 		* Return all data
