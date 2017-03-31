15902ed741b0451e9af457f57620b4359fc136c8
Matheo13 <matheo_jaouen@natural-solutions.eu>
2017-03-31 16:43:29 +0200

    ens, ens spacies

--- themes/bootstrap_aveyron/scripts/script-ens.js
+++ themes/bootstrap_aveyron/scripts/script-ens.js
@@ -18,6 +18,28 @@ jQuery( document ).ready(function() {
         var Shuffle = window.shuffle;
 
 
+        $('.js-btn-filter').on('click', function(e){
+            $('.js-btn-filter').each(function(){
+                $(this).removeClass('active');
+            });
+
+            $(e.currentTarget).addClass('active');
+            
+            var thematique = $(e.currentTarget).attr('thematique');
+            $('.js-figures').find('.js-figure').each(function(){
+                if(thematique == 'all'){
+                    $(this).removeClass('hide');
+                    return;
+                }
+                if($(this).attr('thematique') === thematique){
+                    $(this).removeClass('hide');
+                } else {
+                    $(this).addClass('hide');
+                }
+            });
+        });
+
+
         $('.js-quizz-prop').on('click', function(e){
             $(e.currentTarget).parent().parent().find('.js-quizz-prop').each(function(){
                 $(this).removeClass('active');
