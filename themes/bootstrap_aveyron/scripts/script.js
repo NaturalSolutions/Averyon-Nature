711d8fb9c615985d2a51927ccdf2d3248a567a46
gitkyo <p.delaunay1@gmail.com>
2017-06-08 15:52:24 +0200

    fix bug video

--- themes/bootstrap_aveyron/scripts/script.js
+++ themes/bootstrap_aveyron/scripts/script.js
@@ -37,7 +37,7 @@ jQuery( document ).ready(function() {
 
   };
 
-  var function trackerGoogleAnalitics = function(){
+  var trackerGoogleAnalitics = function(){
 
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
