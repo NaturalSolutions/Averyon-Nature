b5b43f1f304f81953cd32c0381e3760f5c320625
gitkyo <p.delaunay1@gmail.com>
2017-06-08 10:57:27 +0200

    add tracker webmastertools and analitics

--- themes/bootstrap_aveyron/scripts/script.js
+++ themes/bootstrap_aveyron/scripts/script.js
@@ -37,9 +37,22 @@ jQuery( document ).ready(function() {
 
   };
 
+  var function trackerGoogleAnalitics = function(){
+
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-100690129-1', 'auto');
+    ga('send', 'pageview');
+
+  }
+
   window.init = function() {
     toggleNavbar();
     checkHeader();
+    trackerGoogleAnalitics()
   }
 
   init(); // true
